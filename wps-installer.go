4c8a3d2ffd70d494f2a0c3ad1a082c56fea97758
marguerite <i@marguerite.su>
2018-06-01 05:52:34 +0000

    finish copy directory

--- wps-installer.go
+++ wps-installer.go
@@ -8,13 +8,17 @@ package main
 
 import (
   "io"
+  "io/ioutil"
+  "path"
   "path/filepath"
   "net/http"
   "log"
   "os"
   "os/exec"
+  "regexp"
   "runtime"
   "strings"
+  "sort"
 )
 
 func get_arch() string {
@@ -25,8 +29,9 @@ func get_arch() string {
   }
 }
 
-func check_error(e error) {
-  if e != nil { panic(e) }
+func check_error(e error) error {
+  if e != nil { return e }
+  return nil
 }
 
 func download(uri, path string) {
@@ -60,21 +65,149 @@ func unpack(tar, destdir string) {
   log.Println("Done!")
 }
 
-func find_all_dirs(root string) ([]string, error) {
-    var dirs []string
-    err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-        if info.IsDir() {
-            dirs = append(dirs, path)
-        }
-        return nil
-    })
-    return dirs, err
+type byLength []string
+
+func (s byLength) Len() int {
+  return len(s)
+}
+
+func (s byLength) Swap(i, j int) {
+  s[i], s[j] = s[j], s[i]
+}
+
+func (s byLength) Less(i, j int) bool {
+  return len(s[i]) < len(s[j])
+}
+
+func get_all_subdir(dir string) []string {
+  var dirs []string
+  err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+    if info.IsDir() {
+      dirs = append(dirs, path)
+    }
+    return nil
+  })
+  check_error(err)
+  sort.Sort(sort.Reverse(byLength(dirs)))
+  return dirs
 }
 
 func rename_whitespace_in_dir(dir string) {
-  dirs, err := find_all_dirs(dir)
+  dirs := get_all_subdir(dir)
+  re := regexp.MustCompile(`[[:space:]]`)
+
+  for _, path := range dirs {
+    if re.MatchString(path) {
+      os.Rename(path, strings.Replace(path, " ", "_", -1))
+    }
+  }
+}
+
+func absolute_path(relative_path, parentdir string) string {
+  if strings.HasPrefix(relative_path, "/") {
+    return filepath.Clean(relative_path)
+  } else {
+    return filepath.Join(parentdir, relative_path)
+  }
+}
+
+func parentdir(file string) string {
+  abspath, err := filepath.Abs(file)
   check_error(err)
-  log.Println(dirs)
+
+  return path.Dir(abspath)
+}
+
+func follow_symlink(file string) string {
+  var link string
+  dir := parentdir(file)
+
+  link, err := filepath.EvalSymlinks(file)
+  check_error(err)
+
+  return absolute_path(link, dir)
+}
+
+func get_dest(src, dst string) string {
+  dst_info, err := os.Lstat(dst)
+  if err != nil { return dst }
+
+  if dst_info.IsDir() {
+    get_dest(src, filepath.Join(dst, filepath.Base(src)))
+  } else {
+    return dst
+  }
+
+  return dst
+}
+
+func copyfile(src, dst string) error {
+  si, err := os.Lstat(src)
+  check_error(err)
+
+  dst = get_dest(src, dst)
+  if _, err = os.Lstat(dst); err == nil {
+    os.Remove(dst)
+  }
+
+  if si.Mode()&os.ModeSymlink != 0 {
+    orig := follow_symlink(src)
+    dst_parentdir := parentdir(dst)
+    os.Symlink(orig, absolute_path(dst, dst_parentdir))
+  } else {
+    in, err := os.Open(src)
+    check_error(err)
+    defer in.Close()
+
+    out, err := os.Create(dst)
+    check_error(err)
+    defer func() {
+      if e := out.Close(); e != nil {
+        err = e
+      }
+    }()
+
+    _, err = io.Copy(out, in)
+    check_error(err)
+
+    err = out.Sync()
+    check_error(err)
+
+    err = os.Chmod(dst, si.Mode())
+    check_error(err)
+  }
+
+  return err
+}
+
+func copydir(src, dst string) {
+  src_parentdir := parentdir(src)
+  dst_parentdir := parentdir(dst)
+  src = absolute_path(src, src_parentdir)
+  dst = get_dest(src, absolute_path(dst, dst_parentdir))
+
+  src_info, err := os.Stat(src)
+  check_error(err)
+
+  if src_info.IsDir() {
+    if _, err := os.Lstat(dst); err != nil {
+      os.MkdirAll(dst, src_info.Mode())
+    }
+    entries, err := ioutil.ReadDir(src)
+    check_error(err)
+
+    for _, entry := range entries {
+      srcpath := filepath.Join(src, entry.Name())
+      dstpath := filepath.Join(dst, entry.Name())
+      if entry.IsDir() {
+	copydir(srcpath, dstpath)
+      } else {
+        copyfile(srcpath, dstpath)
+      }
+    }
+  } else {
+    copyfile(src, dst)
+  }
 }
 
 func main() {
@@ -89,7 +222,7 @@ func main() {
   //wps_url := "http://kdl1.cache.wps.com/kodl/download/linux/" + wps_alpha + "//" + wps_tar
   //wps_tmp := "/tmp/"
   wps_dir := "wps-office_" + wps_ver + "_" + wps_arch
-  //wps_destdir := "/usr/share/wps-office"
+  wps_destdir := "/usr/share/wps-office"
   //wps_fontdir := "/usr/share/fonts/wps-office"
 
   //if _, err := os.Stat(wps_tar); !os.IsNotExist(err) {
@@ -99,6 +232,10 @@ func main() {
   //}
 
   //unpack(wps_tar, wps_dir)//wps_tmp + wps_dir)
-  //createdir(wps_destdir)
+  createdir(wps_destdir)
   rename_whitespace_in_dir(wps_dir)//wps_tmp + wps_dir)
+
+  log.Println("Copying files...Ultra slow...")
+  copydir("office6", wps_destdir + "/office6")
+
 }
