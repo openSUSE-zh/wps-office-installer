1e253867a33f4e7dd97f8a14f70e7a045022e601
marguerite <i@marguerite.su>
2020-10-19 13:30:33 +0800

    refactor -dep

--- go.mod
+++ go.mod
@@ -1,9 +1,13 @@
 module github.com/openSUSE-zh/wps-office-installer
 
+go 1.15
+
 require (
+	github.com/PuerkitoBio/goquery v1.6.0
 	github.com/kr/text v0.2.0 // indirect
-	github.com/marguerite/util v0.0.0-20200227041307-e5df39a6ecd5
+	github.com/marguerite/go-stdlib v0.0.0-20201019050248-876394b68f10
+	github.com/marguerite/util v0.0.0-20201015054134-9d2feb7630c1 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
-	gopkg.in/yaml.v2 v2.2.8
+	gopkg.in/yaml.v2 v2.3.0
 )
--- wps.go
+++ wps.go
@@ -1,23 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"regexp"
+	"runtime"
 	"strconv"
 	"strings"
+	"sync"
 
-	"github.com/marguerite/util/dir"
-	"github.com/marguerite/util/fileutils"
-	"github.com/marguerite/util/slice"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/marguerite/go-stdlib/dir"
+	"github.com/marguerite/go-stdlib/fileutils"
+	"github.com/marguerite/go-stdlib/slice"
 	yaml "gopkg.in/yaml.v2"
 )
 
@@ -34,28 +38,26 @@ func logname() string {
 	return string(ln)
 }
 
-func loadYAML() (wpsConfig, error) {
+func loadYAML() (config wpsConfig, err error) {
 	file := filepath.Join("/home", logname(), ".config/wps-office/wps.yaml")
-	if _, err := os.Stat(file); os.IsNotExist(err) {
-		if _, err := os.Stat("wps.yaml"); os.IsNotExist(err) {
+	if _, err = os.Stat(file); os.IsNotExist(err) {
+		if _, err = os.Stat("wps.yaml"); os.IsNotExist(err) {
 			file = "/etc/wps-office/wps.yaml"
 		} else {
 			file = "wps.yaml"
 		}
 	}
 
-	c := wpsConfig{}
-
 	b, err := ioutil.ReadFile(file)
 	if err != nil {
-		return c, err
+		return config, err
 	}
 
-	err = yaml.Unmarshal(b, &c)
+	err = yaml.Unmarshal(b, &config)
 	if err != nil {
-		return c, err
+		return config, err
 	}
-	return c, nil
+	return config, nil
 }
 
 func download(src, dest string) error {
@@ -124,7 +126,7 @@ type step struct {
 
 func (s step) install(prefix string) {
 	fmt.Printf("Processing installation - %s\n", s.name)
-	files, _ := dir.Glob(filepath.Join(prefix, s.findpath), "."+s.suffix)
+	files, _ := dir.Glob("."+s.suffix, filepath.Join(prefix, s.findpath))
 	for _, file := range files {
 		fileutils.Copy(file, strings.Replace(file, prefix, "", 1))
 	}
@@ -168,7 +170,7 @@ func main() {
 		}
 
 		// delete all versioned indicators
-		indicators, _ := dir.Glob("/tmp", "^/tmp/wps-office-.*\\.txt$")
+		indicators, _ := dir.Glob("/tmp/wps-office-*.txt", "/tmp")
 		for _, indicator := range indicators {
 			os.Remove(indicator)
 		}
@@ -218,7 +220,7 @@ func main() {
 
 		//install stuff
 		for _, bin := range []string{"wps", "wpp", "et", "wpspdf"} {
-			bin = filepath.Join(prefix, "/usr/bin/"+bin)
+			bin = filepath.Join(prefix, "/usr/bin", bin)
 			replaceBinaryPath(bin, dest)
 			fileutils.Copy(bin, strings.Replace(bin, prefix, "", 1))
 		}
@@ -241,33 +243,43 @@ func main() {
 		fmt.Println("Installation Succeed.")
 		os.Exit(0)
 	} else {
-		if _, err := os.Stat(filepath.Join(directory, "usr")); os.IsNotExist(err) {
+		if _, err := os.Stat(directory); os.IsNotExist(err) {
 			fmt.Println("You must specify -dir option when dep/ghost option is enabled.")
 			os.Exit(1)
 		}
 	}
 
-	office6dir := filepath.Join(directory, "/opt/kingsoft/wps-office/office6")
+	office6dir := filepath.Join(directory, "office6")
 
 	if dep {
 		// dep stuff
 		fmt.Printf("Finding all binaries from %s\n", office6dir)
 		binaries := findBinaries(office6dir)
-		raw := []string{}
+		var libraries []string
 		for _, binary := range binaries {
-			raw = parseRawDepends(binary, raw, directory, binaries)
+			libraries = parseLibraries(binary, libraries, directory, binaries)
 		}
 		fmt.Println("Try resolving library dependencies to openSUSE package names.")
-		depends := findDepends(raw)
-		for _, d := range depends {
-			fmt.Printf("\t%s\n", d)
+		dependencies := findDependencies(libraries)
+		for k := range dependencies {
+			fmt.Printf("%s\n", k)
 		}
 	}
 
 	if ghost {
 		// ghost stuff
-		directories, _ := dir.Ls(office6dir, "dir")
-		files, _ := dir.Ls(office6dir)
+		matches, _ := dir.Ls(office6dir, true, true)
+		var files, directories []string
+
+		for _, m := range matches {
+			i, _ := os.Stat(m)
+			if i.IsDir() {
+				directories = append(directories, m)
+			} else {
+				files = append(files, m)
+			}
+		}
+
 		re := regexp.MustCompile(`[^\/]+\s+[^\/]+`)
 
 		dirs := substitute(directories, filepath.Dir(office6dir), "./usr/share/wps-office")
@@ -296,8 +308,7 @@ func main() {
 	}
 }
 
-func substitute(files []string, orig string, dest string) []string {
-	var res []string
+func substitute(files []string, orig string, dest string) (result []string) {
 	var dest1 string
 	for _, f := range files {
 		if strings.HasPrefix(f, "\"") {
@@ -309,17 +320,16 @@ func substitute(files []string, orig string, dest string) []string {
 				f = strings.TrimPrefix(f, "\"")
 				dest1 = strings.Replace(dest, "%ghost %", "%ghost \"%", 1)
 			}
-			res = append(res, strings.Replace(f, orig, dest1, -1))
+			result = append(result, strings.Replace(f, orig, dest1, -1))
 		} else {
-			res = append(res, strings.Replace(f, orig, dest, -1))
+			result = append(result, strings.Replace(f, orig, dest, -1))
 		}
 	}
-	return res
+	return result
 }
 
-func findBinaries(directory string) []string {
-	files, _ := dir.Ls(directory)
-	binaries := []string{}
+func findBinaries(directory string) (binaries []string) {
+	files, _ := dir.Ls(directory, true, true)
 	for _, file := range files {
 		info, _ := os.Stat(file)
 		// skip zero-bit file
@@ -353,28 +363,31 @@ func findBinaries(directory string) []string {
 	return binaries
 }
 
-func parseRawDepends(file string, depends []string, wpsDir string, selfBinaries []string) []string {
+func parseLibraries(file string, libraries []string, wpsDir string, selfBinaries []string) []string {
 	out, _ := exec.Command("ldd", file).Output()
 
 	for _, line := range strings.Split(string(out), "\n") {
-		dep := parseDepend(line, wpsDir, selfBinaries)
-		if dep != "" {
-			if ok, err := slice.Contains(depends, dep); !ok || err != nil {
-				depends = append(depends, dep)
+		library := parseLibrary(line, wpsDir, selfBinaries)
+		if len(library) > 0 {
+			if ok, err := slice.Contains(libraries, library); !ok || err != nil {
+				if library == "libQtXml.so.4" {
+					fmt.Printf("libQtXml.so.4 required by %s may not find on openSUSE Tumbleweed since Qt4 was deprecated\n", strings.TrimPrefix(file, wpsDir))
+				}
+				libraries = append(libraries, library)
 			}
 		}
 	}
-	return depends
+	return libraries
 }
 
-func parseDepend(str string, wpsDir string, binaries []string) string {
+func parseLibrary(line string, wpsDir string, binaries []string) (library string) {
 	re := regexp.MustCompile(`[[:space:]](.*)[[:space:]]=>[[:space:]]([^\(]+)`)
-	if strings.Contains(str, "=>") {
-		matches := re.FindStringSubmatch(str)
+	if strings.Contains(line, "=>") {
+		matches := re.FindStringSubmatch(line)
 		if !strings.Contains(matches[2], wpsDir) {
 			ok := false
-			for _, bin := range binaries {
-				if strings.Contains(bin, matches[1]) {
+			for _, v := range binaries {
+				if strings.Contains(v, matches[1]) {
 					ok = true
 				}
 			}
@@ -383,32 +396,150 @@ func parseDepend(str string, wpsDir string, binaries []string) string {
 			}
 		}
 	}
-	return ""
+	return library
 }
 
-func findDepends(files []string) []string {
-	var depends []string
-	for _, f := range files {
-		cmd, err := exec.Command("/usr/bin/zypper", "se", "-f", f).Output()
-		if err != nil {
-			re := regexp.MustCompile(`(.*?)(\d+)?\.so\.(\d+)`)
-			matches := re.FindStringSubmatch(f)
-			var res string
-			if len(matches) > 4 {
-				res = strings.Replace(f, ".so.", "-", -1)
-				res = strings.Replace(res, ".", "_", -1)
-			} else {
-				res = strings.Replace(f, ".so.", "", -1)
-			}
-			log.Println(f + " probably resolves to " + res + ", but we couldn't be sure since that package isn't installed. do your own research!")
-			continue
-		}
+// findDependencies find wps runtime dependencies through zypper or rpmfind.net
+func findDependencies(libraries []string) map[string]struct{} {
+	packages := make(map[string]struct{})
+	support64 := support64bit()
+	client := proxyClient()
+	version := openSUSEversion()
 
-		out := strings.Split(string(cmd), "|")
-		d := strings.Replace(out[len(out)-3], " ", "", -1)
-		if ok, err := slice.Contains(depends, d); !ok || err != nil {
-			depends = append(depends, d)
+	wg := sync.WaitGroup{}
+	wg.Add(len(libraries))
+	mux := sync.Mutex{}
+	ch := make(chan struct{}, 5)
+
+	for _, library := range libraries {
+		go func(library, version string, packages map[string]struct{}, client *http.Client, support64 bool) {
+			defer wg.Done()
+			defer func() { <-ch }() // release chan
+			ch <- struct{}{}        // acquire chan
+			out, err := exec.Command("/usr/bin/zypper", "--no-refresh", "se", "-f", "-i", library).Output()
+
+			// query from rpmfind.net
+			if err != nil {
+				pkg := queryPackage(client, library, version, support64)
+				if len(pkg) == 0 {
+					fmt.Printf("Can't resolve %s from rpmfind.net for openSUSE version %s, 64bit %v, may be not supported at all?\n", library, version, support64)
+				} else {
+					if _, ok := packages[pkg]; !ok {
+						mux.Lock()
+						packages[pkg] = struct{}{}
+						mux.Unlock()
+					}
+				}
+			}
+
+			// can find from installed packages
+			scanner := bufio.NewScanner(bytes.NewReader(out))
+			for scanner.Scan() {
+				if strings.HasPrefix(scanner.Text(), "i") {
+					pkg := strings.TrimSpace(strings.Split(scanner.Text(), "|")[1])
+					// don't self requires
+					if pkg == "wps-office" {
+						break
+					}
+					if !support64 || !strings.Contains(pkg, "-32bit") {
+						if _, ok := packages[pkg]; !ok {
+							mux.Lock()
+							packages[pkg] = struct{}{}
+							mux.Unlock()
+							break
+						}
+					}
+				}
+			}
+		}(library, version, packages, client, support64)
+	}
+
+	wg.Wait()
+
+	return packages
+}
+
+// openSUSEversion() return openSUSE version like "Tumbleweed" or "Leap 15.2"
+func openSUSEversion() (version string) {
+	f, err := os.Open("/etc/os-release")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	pretty := "PRETTY_NAME=\"openSUSE "
+	for scanner.Scan() {
+		if strings.HasPrefix(scanner.Text(), pretty) {
+			version = strings.ReplaceAll(strings.ReplaceAll(scanner.Text(), pretty, ""), "\"", "")
 		}
 	}
-	return depends
+	return version
+}
+
+// support64bit if the system is a 64bit platform
+func support64bit() bool {
+	arch := runtime.GOARCH
+	if strings.Contains(arch, "64") || strings.Contains(arch, "390") || strings.Contains(arch, "wasm") {
+		return true
+	}
+	return false
+}
+
+// proxyClient return a http client with "http(s)?_proxy" support
+func proxyClient() *http.Client {
+	return &http.Client{
+		Transport: &http.Transport{
+			Proxy:           http.ProxyFromEnvironment,
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+}
+
+// queryPackage query rpm package name from rpmfind.net
+func queryPackage(c *http.Client, library, version string, support64 bool) (pkg string) {
+	url := "https://rpmfind.net/linux/rpm2html/search.php?query=" + library
+	if support64 {
+		url += "%28%29%2864bit%29"
+	}
+	url += "&submit=Search+...&system=opensuse&arch="
+	if support64 {
+		url += "x86_64"
+	} else {
+		url += "i586"
+	}
+
+	resp, err := c.Get(url)
+	if err != nil {
+		resp.Body.Close()
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var tmp []string
+
+	doc.Find("td").Each(func(i int, s *goquery.Selection) {
+		if i > 11 && i%2 == 0 {
+			tmp = append(tmp, s.Text())
+		}
+	})
+
+	re := regexp.MustCompile(`^(.*?)-\d+\.`)
+
+	for i, v := range tmp {
+		if i > 0 && i%2 != 0 && strings.Contains(v, version) {
+			pkg = tmp[i-1]
+			pkg = re.FindStringSubmatch(pkg)[1]
+			return pkg
+		}
+	}
+
+	return pkg
 }
